96b77f1c70832606a096f4b8883e3fce43707d8a
huoshan12345 <89009143@qq.com>
2016-11-01 01:48:34 +0800

    action工厂

--- src/Utility/Extensions/TypeExtensions.cs
+++ src/Utility/Extensions/TypeExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace Utility.Extensions
@@ -10,6 +11,26 @@ namespace Utility.Extensions
         public static Type GetUnderlyingType(this Type type)
         {
             return Nullable.GetUnderlyingType(type) ?? type;
+        }
+
+        public static bool SequenceAssignableFrom(this IEnumerable<Type> first, IEnumerable<Type> second)
+        {
+            var comparer = EqualityComparer<Type>.Default;
+            using (var e1 = first.GetEnumerator())
+            {
+                using (var e2 = second.GetEnumerator())
+                {
+                    while (e1.MoveNext())
+                    {
+                        if (!e2.MoveNext()) return false;
+                        else if (!(comparer.Equals(e1.Current, e2.Current) || e1.Current.IsAssignableFrom(e2.Current)))
+                            return false;
+                    }
+                    if (e2.MoveNext())
+                        return false;
+                }
+            }
+            return true;
         }
     }
 }
